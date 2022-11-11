f3c051167d7f9ac5251ca557b211c539a0731e70
Ecastillof1987 <99237945+Ecastillof1987@users.noreply.github.com>
2022-11-11 15:10:31 +0000

    actualizacion de ubicaciones

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
-  
+  function recomendacion(){
+    $clima = array("Bogota"=>"frio", "Monteria"=>"calido","Medellin"=>"templado");
+    $ubicacion = array("Guajira"=>"norte","Leticia"=>"sur","Santander"=>"este","Antioquia"=>"oeste");
+    $turismo = array("Santa Marta"=>"mar","Villavicencio"=>"llanos","Riohacha"=>"desierto","Quindio"=>"valle");
+
+    switch("ubicacion"){
+      case "clima":
+        echo array_search("frio",$clima);
+      break;
+      case "ubicacion":
+        echo array_search("norte",$ubicacion);
+      break;
+    }
+  }
+
+  recomendacion()
 ?>
